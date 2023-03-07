0f91f76792f6b2313269216bb4ca2f006411b817
E Valdes <evaldesmrtn@gmail.com>
2023-03-07 18:31:43 +0100

    Solved some compilation errors (mainly syntax errors), doesnt compile yet

--- src/integrators.f90
+++ src/integrators.f90
@@ -7,16 +7,16 @@ module integrators
 
 contains
 
-    subroutine verlet(positions,velocities,dt,nts,cutoff,a_box)
+    subroutine verlet(positions,velocities,dt,nts,cutoff,a_box,temp)
     implicit none
     ! subroutine that integrates newton's equations of motion by means of the verlet algorithm
 
     real(kind=dp), dimension(:,:), intent(inout)    :: positions,velocities
-    real(kind=dp), intent(in)                       :: dt, cutoff, a_box
+    real(kind=dp), intent(in)                       :: dt, cutoff, a_box, temp
     integer(kind=i64), intent(in)                   :: nts
-    real(kind=dp)                                   :: u_ene, dt2, rij_aux
+    real(kind=dp)                                   :: u_ene, kin_ene, pres, dt2, rij(1,1), rij_aux
     real(kind=dp), dimension(:,:), allocatable      :: posold, forces
-    integer(kind=i64)                               :: i,j,np,i_ts,nts
+    integer(kind=i64)                               :: i,j,np,i_ts
 
     np = size(positions, dim=2, kind=i64)
     allocate(forces(3,np), posold(3,np))
@@ -29,7 +29,7 @@ contains
 
     ! opens file to write energies, results for analysis
     open(2, file='energies.dat', status='new')
-    write(2,'(4(a,x))') 'ts','T','U','P' ! timestep, kinetic energy, potential energy, pressure
+    write(2,'(4(a,1x))') 'ts','T','U','P' ! timestep, kinetic energy, potential energy, pressure
     
     posold = positions - velocities*dt ! fabricates positions at timestep -1 using initial velocities
 
@@ -40,17 +40,19 @@ contains
         kin_ene = calc_KE(velocities)
         u_ene = calc_vdw_pbc(positions,cutoff,a_box)
         pres = calc_pressure(a_box, positions, temp, cutoff)
-        write(2, '(4(f12.8,x))') i_ts*dt,kin_ene,u_ene,pres
+        write(2, '(4(f12.8,1x))') i_ts*dt,kin_ene,u_ene,pres
 
         do j=1,np
             do i=1,3
                 rij_aux = positions(i,j)
                 positions(i,j) = 2.0_dp*positions(i,j) - posold(i,j) + forces(i,j)*dt2 ! computes new positions
-                call pbc(positions(i,j), a_box) ! puts single component back to simulation box
+                rij(1,1) = positions(i,j)
+                call pbc(rij, a_box) ! puts single component back to simulation box
+                positions(i,j) = rij(1,1)
                 velocities(i,j) = 0.5_dp*(positions(i,j)-posold(i,j))/dt ! new velocity by component
                 posold(i,j) = rij_aux
             enddo ! xyz components loop
-            write(1,'(a1,x,3(f12.8,x))') 'A', positions(1,j), positions(2,j), positions(3,j)
+            write(1,'(a1,1x,3(f12.8,1x))') 'A', positions(1,j), positions(2,j), positions(3,j)
         enddo ! particles loop
         !write(1,*) ''
 
@@ -60,15 +62,14 @@ contains
     end subroutine
 
 
-    subroutine vel_verlet(positions,velocities,dt,nts,cutoff,a_box)
+    subroutine vel_verlet(positions,velocities,dt,nts,cutoff,a_box,temp)
     implicit none
     ! subroutine that integrates newton's equations of motion by means of the velocity verlet algorithm
 
-    character(*), intent(in)                        :: filename
     real(kind=dp), dimension(:,:), intent(inout)    :: positions,velocities
-    real(kind=dp), intent(in)                       :: dt
+    real(kind=dp), intent(in)                       :: dt, cutoff,a_box, temp
     integer(kind=i64), intent(in)                   :: nts
-    real(kind=dp)                                   :: u_ene,kin_ene,pres,dt2,cutoff,a_box
+    real(kind=dp)                                   :: u_ene,kin_ene,pres,dt2,rij(1,1)
     real(kind=dp), dimension(:,:), allocatable      :: forces
     integer(kind=i64)                               :: i,j,np,i_ts
 
@@ -81,7 +82,7 @@ contains
     !write(1,*) ''
 
     open(2, file='energies.dat', status='new')
-    write(2,'(4(a,x))') 'ts','T','U','P' ! timestep, kinetic energy, potential energy, pressure
+    write(2,'(4(a,1x))') 'ts','T','U','P' ! timestep, kinetic energy, potential energy, pressure
 
     do i_ts = 1,nts
         ! gets current forces and energies, writes output
@@ -89,12 +90,14 @@ contains
         kin_ene = calc_KE(velocities)
         u_ene = calc_vdw_pbc(positions,cutoff,a_box)
         pres = calc_pressure(a_box, positions, temp, cutoff)
-        write(2, '(4(f12.8,x))') i_ts*dt,kin_ene,u_ene,pres
+        write(2, '(4(f12.8,1x))') i_ts*dt,kin_ene,u_ene,pres
 
         do j=1,np
             do i=1,3
-                positions(i,j) = positions(i,j) + velocities(i,j) + 0.5_dp*forces(i,j)*dt2 
-                call pbc(positions(i,j), a_box) ! puts component back to simulation box
+                positions(i,j) = positions(i,j) + velocities(i,j) + 0.5_dp*forces(i,j)*dt2
+                rij(1,1) = positions(i,j) 
+                call pbc(rij, a_box) ! puts component back to simulation box
+                positions(i,j) = rij(1,1)
                 velocities(i,j) = velocities(i,j) + 0.5_dp*forces(i,j)*dt ! velocity by component
             enddo ! xyz components loop
         enddo ! particles loop
