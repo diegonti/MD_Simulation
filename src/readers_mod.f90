71b3364e6cf1329aa44ed312452f62fc96da822b
Marc Alsina <marcalsinac@gmail.com>
2023-03-07 16:45:21 +0100

    Add temperature as input parameter

--- src/readers_mod.f90
+++ src/readers_mod.f90
@@ -6,7 +6,7 @@ contains
 
 subroutine read_nml(param_file, lj_epsilon, lj_sigma, mass, timestep, density, &
     andersen_nu, n_particles, n_steps, write_file, write_stats, gdr_num_bins, &
-    write_frame, sim_name, cell_type, init_velocities)
+    write_frame, sim_name, cell_type, init_velocities, temperature)
     implicit none
     ! Author: Marc Alsina <marcalsinac@gmail.com>
     ! Subroutine to read the simulation parameters from a fortran nml list
@@ -15,7 +15,7 @@ subroutine read_nml(param_file, lj_epsilon, lj_sigma, mass, timestep, density, &
     !   param_file (CHARACTER[*]): The path of the parameter file 
 
     ! In/Out variables
-    real(kind=dp), intent(inout)         :: lj_epsilon, lj_sigma, mass, timestep, density, andersen_nu
+    real(kind=dp), intent(inout)         :: lj_epsilon, lj_sigma, mass, timestep, density, andersen_nu, temperature
     integer(kind=i64), intent(inout)     :: n_particles, n_steps, write_file, write_stats, gdr_num_bins, write_frame
     character(len=2048), intent(inout)   :: sim_name, cell_type, init_velocities
     character(len=2048), intent(in)      :: param_file
@@ -28,7 +28,7 @@ subroutine read_nml(param_file, lj_epsilon, lj_sigma, mass, timestep, density, &
     namelist /PARAMS/ lj_epsilon, lj_sigma, mass, &
     timestep, n_particles, density, n_steps, &
     write_file, write_stats, write_frame, sim_name, &
-    andersen_nu, gdr_num_bins, cell_type, init_velocities
+    andersen_nu, gdr_num_bins, cell_type, init_velocities, temperature
 
     ! Checking if file is present
     inquire(file=trim(param_file), iostat=iost)
