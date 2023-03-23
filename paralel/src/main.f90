0a3b5e79fa105560977aaa7cb98ed838691a3628
diegonti <diegontidoc2000@gmail.com>
2023-03-23 16:15:32 +0100

    initial changes

--- paralel/src/main.f90
+++ paralel/src/main.f90
@@ -1,6 +1,7 @@
 program main
     use, intrinsic :: iso_fortran_env, only: DP => real64, I64 => int64, i32 => int32, input_unit, output_unit, error_unit
     ! Module definitions
+    use mpi
     use            :: initialization, only: changeIUnits, getInitialParams, initializePositions, initializeVelocities
     use            :: testing
     use            :: readers_m,      only: read_nml
@@ -25,40 +26,58 @@ program main
     character(len=2048) :: nml_path, sim_name, log_name, traj_name, rdf_name
     character(len=2048) :: cell_type, init_vel
 
+    ! INITIALIZATION DIEGO
+    integer(kind=i64) :: imin,imax,local_N
+    integer :: ierror,taskid,numproc, MASTER, i, aux
+    integer,  allocatable, dimension(:) :: sendcounts, displs
+
 
     !!! ~ MAIN PROGRAM ~ !!!
 
-    call cpu_time(init_time)
+    ! MPI initialization
+    call MPI_INIT(ierror)
+    call MPI_COMM_RANK(MPI_COMM_WORLD,taskid,ierror)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD,numproc,ierror)
 
-    ! Setting random seed of RNG
-    call random_seed( size=state_size )
-    allocate(state(state_size))
-    state = seed_number
-    call random_seed( put=state )
+    if (taskid == MASTER) then
+        call cpu_time(init_time)
+        
+        
+        ! Setting random seed of RNG
+        call random_seed( size=state_size )
+        allocate(state(state_size))
+        state = seed_number
+        call random_seed( put=state )
 
-    write(output_unit, '(A)') '~ Welcome to MDEMI!! ~'
-    write(output_unit, '(A)') 'Reading input file...'
+        write(output_unit, '(A)') '~ Welcome to MDEMI!! ~'
+        write(output_unit, '(A)') 'Reading input file...'
 
-    call get_command_argument(1, nml_path, status=status_cli)
-    if (status_cli /= 0_i32) then
-        write(error_unit, '(A)') "Cannot understand the input nml file. Please, provide-it as the first argument"
-        stop 1
+        call get_command_argument(1, nml_path, status=status_cli)
+        if (status_cli /= 0_i32) then
+            write(error_unit, '(A)') "Cannot understand the input nml file. Please, provide-it as the first argument"
+            stop 1
+        end if
+    
+        call read_nml(param_file=nml_path, lj_epsilon=lj_epsilon, lj_sigma=lj_sigma, mass=mass, timestep=dt, &
+        density=density, andersen_nu=andersen_nu, n_particles=N, n_steps=n_steps, write_file=write_file, &
+        write_stats=write_stats, gdr_num_bins=gdr_num_bins, write_frame=write_frame, sim_name=sim_name, &
+        cell_type=cell_type, init_velocities=init_vel, temperature=T)
+    
+        ! Trimming character variables in order to avoid blank spaces
+        sim_name = trim(sim_name)
+
+        ! SCATTER
+        
+        write(output_unit, '(A)') 'Successfully loaded parameter file, starting simulation'
     end if
 
-    call read_nml(param_file=nml_path, lj_epsilon=lj_epsilon, lj_sigma=lj_sigma, mass=mass, timestep=dt, &
-    density=density, andersen_nu=andersen_nu, n_particles=N, n_steps=n_steps, write_file=write_file, &
-    write_stats=write_stats, gdr_num_bins=gdr_num_bins, write_frame=write_frame, sim_name=sim_name, &
-    cell_type=cell_type, init_velocities=init_vel, temperature=T)
 
-    ! Trimming character variables in order to avoid blank spaces
-    sim_name = trim(sim_name)
-
-
-    write(output_unit, '(A)') 'Successfully loaded parameter file, starting simulation'
 
     ! ~ Memmory allocation ~
     allocate(r(3,N))
     allocate(v(3,N))
+    allocate(sendcounts(numproc))
+    allocate(displs(numproc))
 
     ! Opening files
     ! log_unit  -> file where the simulation time, energy, instant temperature, etc.. will be placed
@@ -81,12 +100,18 @@ program main
     ! ~ Initialization of the system ~
     call changeIUnits(lj_epsilon,lj_sigma,mass,density,dt,T)
     call getInitialParams(trim(cell_type),N,density,M,L,a)
-    call initializePositions(M,a,r,trim(cell_type))
+
+    call divide_positions(taskid,numproc,N, sendcounts,displs,imin,imax,local_N)
+    ! call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    call initializePositions(M,a,r,trim(cell_type),imin,imax,sendcounts,displs)
+
     call initializeVelocities(T,v,init_vel)
 
+    ! call testMatrix(r) for MASTER
+
     ! ~ Starting the trajectory of the system ~
-    call mainLoop(log_unit, traj_unit, rdf_unit, lj_epsilon, lj_sigma, mass, &
-    n_steps, dt, L, T, andersen_nu, 0.5_dp*L, gdr_num_bins, r, v, write_stats, write_frame)    
+    ! call mainLoop(log_unit, traj_unit, rdf_unit, lj_epsilon, lj_sigma, mass, &
+    ! n_steps, dt, L, T, andersen_nu, 0.5_dp*L, gdr_num_bins, r, v, write_stats, write_frame)    
 
 
     ! ~ Closing files ~
@@ -97,6 +122,9 @@ program main
     ! ~ Memmory deallocation ~
     deallocate(r)
     deallocate(v)
+    deallocate(sendcounts)
+    deallocate(displs)
+
     
     ! ~ Program finalization ~
     call cpu_time(end_time)
